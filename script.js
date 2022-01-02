6d91e271c408169c7ad10dd9e4f87cc12a3dccdc
Arlisson N. Costa <arlisson314@gmail.com>
2022-01-02 12:59:27 -0500

    adiciona a funcao seletorDeCores

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let corSeletor = document.querySelector('.selected');
-let corSelecionada = corSeletor.style.backgroundcolor;
-let corElemento = document.querySelector('.color');
+let corSelecionada = corSeletor.style.backgroundColor;
+let corElemento = document.querySelectorAll('.color');
 
 function seletorDeCores(event) {
-    const corAntigaSeletor = corSeletor;
-    corAntigaSeletor.classList.remove('selected');
-
+    const corAntigaSeletor = corSeletor.classList.remove('selected');
+    const corNovaSeletor = event.target.classList.add('selected');
+    corSeletor = corNovaSeletor;
+    corSelecionada = corNovaSeletor.style.backgroundColor;
 };
