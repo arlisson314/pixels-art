1491868b03d34df3620ac0c163af689e8609b319
Arlisson N. Costa <arlisson314@gmail.com>
2022-07-02 17:51:29 -0500

    feat requisito 10

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+const body = document.getElementsByTagName('body')[0];
+body.style.background = 'linear-gradient(88.15deg, #2F2325 16.62%, #8E5D52 85.61%)';
+
 let corSeletor = document.querySelector('.selected');
-let body = document.getElementsByTagName('body')[0];
-body.style.background = 'linear-gradient(68.15deg, #2F2325 16.62%, #8E5D52 85.61%)';
+const allColors = document.getElementsByClassName('color');
+const pixelElemento = document.getElementsByClassName('pixel');
+const btnClearBoard = document.getElementById('clear-board');
+const input = document.getElementById('board-size');
+const btnForm = document.getElementById('generate-board');
 let corSelecionada = corSeletor.style.backgroundColor;
-const corElemento = document.querySelectorAll('.color');
+
+function inputForm(event) {
+  event.preventDefault();
+  const pixelContainer = document.getElementById('pixel-board');
+  pixelContainer.innerHTML = '';
+  if (input.value !== '') {
+    for (let i = 1; i <= input.value ** 2; i += 1) {
+      const div = document.createElement('div');
+      div.classList.add('pixel');
+      div.style.background = 'white';
+      pixelContainer.appendChild(div);
+    }
+  } else {
+    alert('Board inválido!');
+  }
+  input.value = '';
+}
+btnForm.addEventListener('click', inputForm);
 
 function seletorDeCores(event) {
   const corAntigaSeletor = corSeletor;
@@ -12,14 +35,12 @@ function seletorDeCores(event) {
   corNovoSeletor.classList.add('selected');
 
   corSeletor = corNovoSeletor;
-  corSelecionada = corNovoSeletor.style.backgroundColor;
+  corSelecionada = corSeletor.style.backgroundColor;
 }
 
-for (let index = 0; index < corElemento.length; index += 1) {
-  corElemento[index].addEventListener('click', seletorDeCores);
+for (let index = 0; index < allColors.length; index += 1) {
+  allColors[index].addEventListener('click', seletorDeCores);
 }
-
-const pixelElemento = document.querySelectorAll('.pixel');
 
 function paintPixel(event) {
   const pixelElementoPaint = event.target;
@@ -30,12 +51,9 @@ for (let index = 0; index < pixelElemento.length; index += 1) {
   pixelElemento[index].addEventListener('click', paintPixel);
 }
 
-const clearBoard = document.getElementById('clear-board');
-
 function limpar() {
   for (let index = 0; index < pixelElemento.length; index += 1) {
     pixelElemento[index].style.backgroundColor = 'white';
   }
 }
-
-clearBoard.addEventListener('click', limpar);
+btnClearBoard.addEventListener('click', limpar);
