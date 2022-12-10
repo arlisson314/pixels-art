795d22e4b36f2eb532171b1a3fd69fa26fe8b87f
Arlisson N. Costa <arlisson314@gmail.com>
2022-12-10 10:31:11 -0500

    feat: implementa metodo  gerador cores aleatorias

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const body = document.getElementsByTagName('body')[0];
 body.style.background = 'linear-gradient(88.15deg, #2F2325 16.62%, #8E5D52 85.61%)';
 
-let corSeletor = document.querySelector('.selected');
 const allColors = document.getElementsByClassName('color');
 const pixelElemento = document.getElementsByClassName('pixel');
 const btnClearBoard = document.getElementById('clear-board');
 const input = document.getElementById('board-size');
 const btnForm = document.getElementById('generate-board');
+const btnRefreshColors = document.getElementById('refresh')
+
+let corSeletor = document.querySelector('.selected');
+let colorPalet = document.getElementsByClassName('color')
 let corSelecionada = corSeletor.style.backgroundColor;
+
+function geradorDeCores() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function mudarCores() {
+  return Array.from(colorPalet).map(({style}) => style.backgroundColor = geradorDeCores());
+}
+btnRefreshColors.addEventListener('click', mudarCores)
 
 function seletorDeCores(event) {
   const corAntigaSeletor = corSeletor;
@@ -30,7 +46,6 @@ function paintPixel(event) {
 }
 
 for (let index = 0; index < pixelElemento.length; index += 1) {
-  console.log(pixelElemento[index]);
   pixelElemento[index].addEventListener('click', paintPixel);
 }
 
@@ -44,8 +59,9 @@ btnClearBoard.addEventListener('click', limpar);
 function inputForm(e) {
   e.preventDefault();
   const pixelContainer = document.getElementById('pixel-board');
-  pixelContainer.innerHTML = '';
-  if (input.value !== '') {
+  input.value > 0 ? pixelContainer.innerHTML = '' : pixelContainer;
+
+  if (input.value !== '' && input.value > 0) {
     for (let i = 1; i <= input.value ** 2; i += 1) {
       const div = document.createElement('div');
       div.classList.add('pixel');
@@ -56,5 +72,4 @@ function inputForm(e) {
   }
   input.value = '';
 }
-
 btnForm.addEventListener('click', inputForm);
